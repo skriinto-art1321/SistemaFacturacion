7b25dd026589876f2badbdb8a1c6db0e3c62291f
Ronaldo <skri_into@hotmail.com>
2026-06-14 16:51:48 -0500

    crear modelos de negocio

---
+++ src/model/Cliente.java
@@ -0,0 +1,24 @@
+public class Cliente {
+
+    private String dni;
+    private String nombre;
+    private String direccion;
+
+    public Cliente(String dni, String nombre, String direccion) {
+        this.dni = dni;
+        this.nombre = nombre;
+        this.direccion = direccion;
+    }
+
+    public String getDni() {
+        return dni;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+}
